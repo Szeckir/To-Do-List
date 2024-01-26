2859a84589d993fff3f79a23862026fbab4c9844
Thomaz Szeckir <thomazszeckir@gmail.com>
2024-01-26 10:09:28 -0300

    Adicionado script JS para adição de tasks, personalizacao de poucas coisas no css e modificacao nome classes html

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const addTaskBt = document.querySelector('.add-button')
-
+const addTaskBt = document.querySelector('#add-button')
+const inputBox = document.querySelector('#input-box')
+const taskList = document.querySelector('#task-list')
 
 addTaskBt.addEventListener('click', () => {
     addTask()
 })
 
 function addTask() {
-    alert('testando')
+    if(inputBox.value === '') {
+        alert('Você precisa digitar algo')
+    } else {
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value
+        taskList.appendChild(li)
+    }
 }
